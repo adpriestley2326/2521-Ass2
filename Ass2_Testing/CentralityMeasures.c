6b6078ab7eaa00cf0ad5d315ce0158beb5a6bed9
Adam Priestley <adam.priestley@student.unsw.edu.au>
2019-04-17 12:35:40 +1000

    Either did degree centrality functions, or just debugging git - not sure which

--- Ass2_Testing/CentralityMeasures.c
+++ Ass2_Testing/CentralityMeasures.c
@@ -1,42 +1,62 @@
-// Graph ADT interface for Ass2 (COMP2521)
-#include "CentralityMeasures.h"
-#include "Dijkstra.h"
-#include "PQ.h"
-#include <stdlib.h>
+// Assignment 2 Task 3 - Centrality measures for social network analysis
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-NodeValues outDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-NodeValues inDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-NodeValues degreeCentrality(Graph g) {
-	NodeValues throwAway = {0};
-	return throwAway;
-}
+#include "CentralityMeasures.h"
 
-NodeValues closenessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
+static double AdjListDegree (AdjList l) {
+    double counter = 0;
+    for (AdjList curr = l; curr != NULL; curr = curr->next) {
+        counter++;
+    }
+    return counter;
+} 
 
-NodeValues betweennessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-
-NodeValues betweennessCentralityNormalised(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+NodeValues outDegreeCentrality(Graph g) {
+    NodeValues outD;
+    outD.noNodes = numVertices (g);
+    outD.values = malloc (outD.noNodes * sizeof double);
+    AdjList outConns = NULL;
+    for (int i = 0; i < outD.noNodes; i++) {
+        outConns = outIncident (g, i);
+        outD.values[i] = AdjListDegree (outConns);
+    }
+    return outD;
 }
 
-void showNodeValues(NodeValues values){
-
+NodeValues inDegreeCentrality(Graph) {
+    NodeValues inD;
+    inD.noNodes = numVertices (g);
+    inD.values = malloc (inD.noNodes * sizeof double);
+    AdjList inConns = NULL;
+    for (int i = 0; i < inD.noNodes; i++) {
+        inConns = inIncident (g, i);
+        inD.values[i] = AdjListDegree (inConns);
+    }
+    return inD;
 }
 
-void freeNodeValues(NodeValues values){
-
+NodeValues degreeCentrality(Graph) { // for undirected graph
+    NodeValues degrees;
+    degrees.noNodes = numVertices (g);
+    degrees.values = malloc (degrees.noNodes * sizeof double);
+    AdjList outConns, inConns;
+    outConns = inConns = NULL;
+    for (int i = 0; i < degrees.noNodes; i++) {
+        outConns = outIncident (g, i);
+        inConns = inIncident (g, i);
+        degrees.values[i] = AdjListDegree (outConns) + AdjListDegree (inConns);
+    }
+    return degrees;
 }
+
+NodeValues closenessCentrality(Graph) {}
+
+NodeValues betweennessCentrality(Graph) {}
+NodeValues betweennessCentralityNormalised(Graph) {}
+
+
+void  showNodeValues(NodeValues) {}
+void  freeNodeValues(NodeValues) {}
