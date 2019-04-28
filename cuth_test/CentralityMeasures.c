446b09148f645f48e6da7c5e835a7513a768e871
Cuthbert Zhong <z5206138@ad.unsw.edu.au>
2019-04-28 19:46:11 +1000

    Tests for OSX added but some not working

--- cuth_test/CentralityMeasures.c
+++ cuth_test/CentralityMeasures.c
@@ -1,42 +1,159 @@
-// Graph ADT interface for Ass2 (COMP2521)
+// Assignment 2 Task 3 - Centrality measures for social network analysis
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
 #include "CentralityMeasures.h"
 #include "Dijkstra.h"
-#include "PQ.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-NodeValues outDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-NodeValues inDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-NodeValues degreeCentrality(Graph g) {
-	NodeValues throwAway = {0};
-	return throwAway;
-}
+static double AdjListDegree (AdjList l) {
+    double counter = 0;
+    for (AdjList curr = l; curr != NULL; curr = curr->next) {
+        counter++;
+    }
+    return counter;
+} 
 
-NodeValues closenessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
-}
-
-NodeValues betweennessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+NodeValues outDegreeCentrality(Graph g) {
+    NodeValues outD;
+    outD.noNodes = numVerticies (g);
+    //printf ("n vertices in graph: %d\n", outD.noNodes);
+    outD.values = malloc (outD.noNodes * sizeof (double));
+    AdjList outConns = NULL;
+    for (int i = 0; i < outD.noNodes; i++) {
+        outConns = outIncident (g, i);
+        outD.values[i] = AdjListDegree (outConns);
+    }
+    return outD;
 }
 
-NodeValues betweennessCentralityNormalised(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+NodeValues inDegreeCentrality(Graph g) {
+    NodeValues inD;
+    inD.noNodes = numVerticies (g);
+    inD.values = malloc (inD.noNodes * sizeof (double));
+    AdjList inConns = NULL;
+    //printf ("Centrality found for nodes: ");
+    for (int i = 0; i < inD.noNodes; i++) {
+        inConns = inIncident (g, i);
+        inD.values[i] = AdjListDegree (inConns);
+        //printf ("%d ", i);
+    }
+    return inD;
 }
 
-void showNodeValues(NodeValues values){
-
+NodeValues degreeCentrality(Graph g) { // for undirected graph
+    NodeValues degrees;
+    degrees.noNodes = numVerticies (g);
+    degrees.values = malloc (degrees.noNodes * sizeof (double));
+    AdjList outConns, inConns;
+    outConns = inConns = NULL;
+    for (int i = 0; i < degrees.noNodes; i++) {
+        outConns = outIncident (g, i);
+        inConns = inIncident (g, i);
+        degrees.values[i] = AdjListDegree (outConns) + AdjListDegree (inConns);
+    }
+    return degrees;
 }
 
-void freeNodeValues(NodeValues values){
+NodeValues closenessCentrality(Graph g) {
+    NodeValues out;
+    out.noNodes = numVerticies (g); // number of nodes in Graph
+    double N = (double) out.noNodes; 
+    out.values = malloc (out.noNodes * sizeof (double));
+    
+    double n = 0;  // number of reachable nodes from u (including u)
+    double pathSum = 0;
+    int dist = 0;
+    for (Vertex u = 0; u < N; u++) {
+        n = 0;
+        pathSum = 0;
+        ShortestPaths uPaths = dijkstra (g, u);
+        for (Vertex v = 0; v < N; v++) {
+            dist = uPaths.dist[v];
+            if (dist > 0 || v == u) {
+                n++;
+                pathSum += dist;
+            }
+        }
+        // Apply formula from spec -> out.values[u]
+        if (n == 1) out.values[u] = 0;
+        else out.values[u] = (n-1) / (N-1) * (n-1) / pathSum;
+    }
+    return out;
+}
 
+static int nShortestPaths (Graph g, Vertex s, Vertex t) {
+    if (s == t) return 1;
+    int nsp = 0; //number of shortest paths from s to t
+    ShortestPaths sPaths = dijkstra (g, s);
+    for (PredNode *currN = sPaths.pred[t]; currN; currN = currN->next) {
+        nsp += nShortestPaths (g, s, currN->v);
+    }
+    return nsp;
+}
+
+static int nPathsThrough (Graph g, Vertex s, Vertex t, Vertex v) {
+    if (t == v) return nShortestPaths (g, s, t);
+    if (t == s) return 0;
+    ShortestPaths sPaths = dijkstra (g, s);
+    int nPaths = 0;
+    for (PredNode *curr = sPaths.pred[t]; curr; curr = curr->next) {
+        nPaths += nPathsThrough (g, s, curr->v, v);
+    }
+    return nPaths;
+}
+
+NodeValues betweennessCentrality (Graph g) {
+    NodeValues out;
+    out.noNodes = numVerticies (g);
+    out.values = malloc (out.noNodes * sizeof (double));
+    double numPaths = 0;
+    double numPathsThrough = 0;
+    for (Vertex s = 0; s < out.noNodes; s++) {
+        //printf ("\nAnalysing routes from %d to...\n", s);
+        for (Vertex t = 0; t < out.noNodes; t++) {
+            //printf ("node %d, through: \n  ", t);
+            numPaths = (double) nShortestPaths (g, s, t);
+            for (Vertex v = 0; v < out.noNodes; v++) {
+                //printf ("%d ", v);
+                if (v == s || v == t || numPaths == 0) continue;
+                numPathsThrough = (double) nPathsThrough (g, s, t, v);
+                out.values[v] += numPathsThrough / numPaths;
+            }
+            //printf ("\n");
+        }
+        //printf ("\n... analysed\n");
+    }
+    return out;
+}
+
+NodeValues betweennessCentralityNormalised (Graph g) {
+    int n = numVerticies (g);
+    NodeValues out;
+    out.noNodes = n;
+    //printf ("n vertices in graph: %d\n", out.noNodes);
+    out.values = malloc (out.noNodes * sizeof (double));
+    NodeValues betweenness = betweennessCentrality (g);
+    for (Vertex v = 0; v < n; v++) {
+        if (n < 3)
+            out.values[v] = 0;
+        else
+            out.values[v] = betweenness.values[v] / ((double)(n-1) * (double)(n-2));
+    }
+    freeNodeValues (betweenness);
+    return out;
+}
+
+
+void  showNodeValues(NodeValues n) {
+    for (Vertex v = 0; v < n.noNodes; v++) {
+        printf ("%d: %f\n", v, n.values[v]);
+    }
+    return;
+}
+void  freeNodeValues(NodeValues n) {
+    free (n.values);
+    return;
 }
